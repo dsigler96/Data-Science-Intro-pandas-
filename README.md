116bf8bf4c3cc2f98659df93e72e3a2296fa7e2f
dsigler96 <94070628+dsigler96@users.noreply.github.com>
2022-10-01 22:42:19 -0600

    final code commit for now

--- README.md
+++ README.md
@@ -1,42 +1,27 @@
 # Overview
 
-{Important!  Do not say in this section that this is college assignment.  Talk about what you are trying to accomplish as a software engineer to further your learning.}
-
-{Provide a description of the data set that you are analyzing.  Include the link of where you obtained the data.}
-
-{Describe your purpose for writing this software to analyze the data.}
-
-{Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the data set, the questions and answers, the code running and a walkthrough of the code.}
-
-REQUIREMENTS:
-Identify the two questions about the dataset on your own. You might change your questions after you explore the data further.
-
-Write a program in any programming language using a statistics library (e.g. R or Python Pandas) to analyze the dataset and provide an answer to your questions.
-
-You must demonstrate the ability to do one or more of the following to achieve each answer: filter, sort, aggregate (sum, average, count), or data conversion.
-
-While there is no right or wrong answer, your answer should be justified by your analysis.
+This is going to be an evolving project as it started as an intro to Panas but slowly evolved into a crash course on machine learning.  This subject has always interested me because I like the idea of analyzing relationships and coming to conclusions of how different variables relate to each other while being as objective as possible.  I decided to start with the data set enclosed in the repository because it would provide evolution to a machine learning test with the train by learning things related to the price of the house.  
 
 [Software Demo Video](http://youtube.link.goes.here)
 
 # Data Analysis Results
 
-{List the questions and the answers you found by doing this analysis.}
+The first question I had was which variable had the best linear relationship with Sales Price, and that seemed to be 'GrLivArea' or General Living Area.
+
+The second question I had was were there any transformations that would apply to the Sales Price and other key variables that had relationships that could make the data have a stronger linear relationship.  Eventually, I applied a logarithmic transformation and that made the dataset follow much closer to the normal curve.  Applying these transformations to the variables that had a strong corelation should help for predicting better house prices after I figure out what transformation I need to apply to TotalBsmtSF so that the computer can draw from 3 variables rather than 2 to predict house prices in the test.csv
 
 # Development Environment
 
-{Describe the tools that you used to develop the software}
-
-{Describe the programming language that you used and any libraries.}
+To develop this program, I used VSCode and Python.  The libraries I used are pandas, numpy, mathplotlib, seaborn, and a few others for select commands that I needed.
 
 # Useful Websites
 
 * [Learn Data Science](https://www.learndatasci.com/tutorials/predicting-housing-prices-linear-regression-using-python-pandas-statsmodels/)
 * [Towards Data Science](https://towardsdatascience.com/predicting-house-prices-with-linear-regression-machine-learning-from-scratch-part-ii-47a0238aeac1)
+* Youtube - Various tutorials
 
 # Future Work
 
-{Make a list of things that you need to fix, improve, and add in the future.}
-* Make the graphing cleaner - It looks very basic
-* Item 2
-* Item 3
+* Find the transformations for TotalBsmtSF to create a linear relationship between it and SalePrice
+* Once that comparison is made, compare it with the Test Data
+* Be objective and look to see where I have made some oversights!
--- housingprices.py
+++ housingprices.py
@@ -12,7 +12,6 @@ import numpy as np
 from scipy.stats import norm
 from sklearn.preprocessing import StandardScaler
 from scipy import stats
-import matplotlib_inline
 
 # Create Train and Test dataframe
 dfTrain = pd.read_csv('train.csv')
@@ -63,18 +62,87 @@ plt.show()
 
 # A Correlation Matrix would show that very quickly what variables have strong relationships with each other
 corMat = dfTrain.corr()
-f, ax = plt.subplots(figsize = (9, 12))
+f, ax  = plt.subplots(figsize = (9, 12))
 sns.heatmap(corMat, vmax = .8, square = True)
 plt.show()
 
 # Now I wanted to show a SalePrice Correlation Matrix after looking at their individual relationships with variables that stuck out
-i = 10
+i    = 10
 cols = corMat.nlargest(i, 'SalePrice')['SalePrice'].index
-cM = np.corrcoef(dfTrain[cols].values.T)
+cM   = np.corrcoef(dfTrain[cols].values.T)
 sns.set(font_scale = 1.5)
-hm = sns.heatmap(cM, cbar = True, annot = True, square = True, fmt = '.2f', annot_kws = {'size' : 10}, yticklabels = cols.values, xticklabels = cols.values)
+hm   = sns.heatmap(cM, cbar = True, annot = True, square = True, fmt = '.2f', annot_kws = {'size' : 10}, yticklabels = cols.values, xticklabels = cols.values)
 plt.show()
 
+# This is a graph I may have gone overboard with, but its a scatterplot of each block of the previous Correlation Matrix
+sns.set()
+cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+sns.pairplot(dfTrain[cols], height = 2.5)
+plt.show()
 
+# Now I wanted to take a look at what data is missing so I know what to ignore and throw out
+total        = dfTrain.isnull().sum().sort_values(ascending=False)
+percent      = (dfTrain.isnull().sum()/dfTrain.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+print(missing_data.head(20))
 
-#51
+# From this, I can throw out PoolQC, MiscFeature, Alley, Fence, FireplaceQu, and LotFrontage because they are missing more than 15% each
+dfTrain = dfTrain.drop((missing_data[missing_data['Total'] > 1]).index,1)
+dfTrain = dfTrain.drop(dfTrain.loc[dfTrain['Electrical'].isnull()].index)
+dfTrain.isnull().sum().max()
+
+# Now to find the Outliers!
+salePriceScaled = StandardScaler().fit_transform(dfTrain['SalePrice'][:,np.newaxis])
+low_range       = salePriceScaled[salePriceScaled[:,0].argsort()][:10]
+high_range      = salePriceScaled[salePriceScaled[:,0].argsort()][-10:]
+print('outer range (low) of the distribution:')
+print(f'{low_range}\n')
+print('outer range (high) of the distribution:')
+print(f'{high_range}\n')
+
+# And remove them
+dfTrain.sort_values(by = 'GrLivArea', ascending = False)[:2]
+dfTrain = dfTrain.drop(dfTrain[dfTrain['Id'] == 1299].index)
+dfTrain = dfTrain.drop(dfTrain[dfTrain['Id'] == 524].index)
+
+# Now lets look at living area without those outliers
+var = 'GrLivArea'
+data = pd.concat([dfTrain['SalePrice'], dfTrain[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000))
+plt.show()
+
+var = 'TotalBsmtSF'
+data = pd.concat([dfTrain['SalePrice'], dfTrain[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000))
+plt.show()
+
+# With the modified data, lets replot the sale price histogram with the normal graph
+sns.distplot(dfTrain['SalePrice'], fit=norm)
+fig = plt.figure()
+res = stats.probplot(dfTrain['SalePrice'], plot=plt)
+plt.show()
+
+# Since the probability plot was showing to be exponential and not linear, I wanted to try a log transformation to see what that did to the data set
+dfTrain['SalePrice'] = np.log(dfTrain['SalePrice'])
+sns.distplot(dfTrain['SalePrice'], fit=norm)
+fig = plt.figure()
+res = stats.probplot(dfTrain['SalePrice'], plot=plt)
+plt.show()
+
+# That transformation looked great, so lets do the same on GrLivArea, showing the before and after
+sns.distplot(dfTrain['GrLivArea'], fit=norm)
+fig = plt.figure()
+res = stats.probplot(dfTrain['GrLivArea'], plot=plt)
+plt.show()
+
+dfTrain['GrLivArea'] = np.log(dfTrain['GrLivArea'])
+sns.distplot(dfTrain['GrLivArea'], fit=norm)
+fig = plt.figure()
+res = stats.probplot(dfTrain['GrLivArea'], plot=plt)
+plt.show()
+
+# TotalBsmtSF before any transformations
+sns.distplot(dfTrain['TotalBsmtSF'], fit=norm)
+fig = plt.figure()
+res = stats.probplot(dfTrain['TotalBsmtSF'], plot=plt)
+plt.show()
